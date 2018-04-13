c2eb42e2c097747d1f946f79abfa6cf59ed4bba2
viva121 <ka.vadim@gmail.com>
2018-04-13 22:33:23 +0300

    login-logout

--- login/loginCtrl.js
+++ login/loginCtrl.js
@@ -2,10 +2,16 @@ app.controller('loginCtrl', function($scope, activeUserService, $log, $location)
 
     $scope.invalidCredentails = false;
 
+    $scope.hideAlert = function() {
+        $scope.invalidCredentails = false;
+    }
+    
+
     $scope.login = function() {
         // TODO: Here you should disable the login button until there is a response from the service
-
+        $scope.loginDisabled = true;
         activeUserService.login($scope.email, $scope.pwd).then(function(successLogin) {
+            $scope.loginDisabled = false;
             if (successLogin) {
                 $location.path("/dashboard");
             } else {
